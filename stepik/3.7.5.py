5ead49d1c618b256b1353147a3aaffd8ec513735
Dmitry <saiferdima@gmail.com>
2019-03-04 02:12:43 +0200

    3.7.5 update

--- stepik/3.7.5.py
+++ stepik/3.7.5.py
@@ -1,18 +1,28 @@
 result = {}
 result_string = ''
-for i in range(1,12):
+spliter = {}
+for i in range(1, 12):
+    spliter[str(i)] = 0
+
+for i in range(1, 12):
     result[str(i)] = '-'
 # print(result)
 with open('input.txt', 'r') as file:
     for line in file:
         data = line.strip().split('\t')
-        if (result[data[0]] == '-'):
-            result[data[0]] = float(data[2])
+        print(data[0])
+        if result[data[0]] == '-':
+            result[data[0]] = int(data[2])
+            spliter[data[0]] = 1
         else:
-            result[data[0]] =(float(result[data[0]]) + float(data[2])) / 2
+            result[data[0]] = result[data[0]] + int(data[2])
+            spliter[data[0]] = spliter[data[0]] + 1
 
 with open('output.txt', 'w') as file:
     for key in result:
-        result_string = result_string + (str(key)+ ' '+ str(result[key]) + '\n')
+        if result[key] != '-':
+            result_string = result_string + (str(key) + ' ' + str(result[key] / spliter[key]) + '\n')
+        else:
+            result_string = result_string + (str(key) + ' ' + '-' + '\n')
 
     file.write(result_string)
