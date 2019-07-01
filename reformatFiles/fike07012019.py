9b02cf952aee7540d3c980dbccd9577b8f2b886d
saiferdima <saiferdima@gmail.com>
2019-07-01 17:47:51 +0300

    Added scripts for reformat selectors in files

---
+++ reformatFiles/fike07012019.py
@@ -0,0 +1,37 @@
+
+input_string = ''
+result = ''
+
+def newFormat(line):
+    temp_element = line[:line.find('=')+1]+' buildElement'
+    if line.find('.all')>0:
+        temp_element = temp_element + 's'        
+    temp_element = temp_element + '(\''
+    temp_element = temp_element + line[line.find('this.')+5:line.find(' = ')]
+    temp_element = temp_element + "\', \'"
+    line = line[line.find('by.')+3:]
+    temp_element = temp_element + line[:line.find('(')] + '\' ,'
+    temp_element = temp_element + line[line.find('(')+1: line.find(')')] + ')'   
+    return temp_element
+
+
+    
+
+    
+with open ('input.txt','r') as file:
+    for line in file:
+        if line.find('= element')>0:
+            input_string = input_string+line.strip()
+            input_string = input_string+'\n'
+            print (newFormat(line))
+            result = result + '\n'+ newFormat(line)
+
+
+with open ('output.txt','w') as file2:
+    file2.write(result)
+    
+
+
+
+    
+
